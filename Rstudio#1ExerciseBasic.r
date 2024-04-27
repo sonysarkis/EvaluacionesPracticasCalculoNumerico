daea589bc82674f045213901bf3d34c02139193a
Sony Gómez <sonygomez6@gmail.com>
2024-04-27 17:55:33 -0400

    Primeros pasos en RSTUDIO. Input de nombre y apellido

---
+++ Rstudio#1ExerciseBasic.r
@@ -0,0 +1,6 @@
+# Pedimos nombre y apellido al usuario
+nombre <- readline(prompt =paste("Ingrese su primer nombre: "))
+apellido <- readline(prompt = paste("Ingrese su primer apellido: "))
+
+# Mostramos el saludo
+print(paste("Hola", nombre, apellido,"¡Un gusto saludarle!"))
